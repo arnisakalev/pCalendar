984a7dd7ed54282729fed66d1fc48cb8987d258f
Arnis Kharchenko <arnisakalev@gmail.com>
2015-10-28 00:49:56 +0300

    Добавил строку подключения

--- pCalendarService/CalendarModelTest/Program.cs
+++ pCalendarService/CalendarModelTest/Program.cs
@@ -10,37 +10,14 @@ namespace CalendarModelTest
     {
         static void Main(string[] args)
         {
-            //Data Source=(localdb)\\mssqllocaldb;Initial Catalog=ProductionCalendarModel.CalendarContext;Integrated Security=True;MultipleActiveResultSets=True
-
             using (var db = new ProductionCalendarModel.CalendarContext())
             {
-                //Console.Write("Press any key");
-                //var cName = Console.ReadLine();
+                var q = from c in db.Countries select c;
 
-                //var newCalendar = new ProductionCalendarModel.country() { CountryName = "Россия" };
-                //var newCalendar1 = new ProductionCalendarModel.country() { CountryName = "Беларусь" };
-
-                //db.Countries.Add(newCalendar);
-                //db.Countries.Add(newCalendar1);
-
-                //var q = from c in db.Countries select c;
-
-                //foreach (var item in q)
-                //{
-                //    Console.WriteLine(item.CountryName);
-                //}
-
-                //Console.ReadLine();
-
-                //try
-                //{
-                //    db.SaveChanges();
-                //}
-                //catch (Exception ex)
-                //{
-                //    Console.WriteLine(ex.ToString());
-                //}                
-
+                foreach (var item in q)
+                {
+                    Console.WriteLine(item.CountryName);
+                }
             }
         }
     }
