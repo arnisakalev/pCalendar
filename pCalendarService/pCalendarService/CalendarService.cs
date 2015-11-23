fe86e2b4eeaf98c830742928109d06456a5dbc31
Arnis Kharchenko <arnisakalev@gmail.com>
2015-11-24 00:04:43 +0300

    dddd

--- pCalendarService/pCalendarService/CalendarService.cs
+++ pCalendarService/pCalendarService/CalendarService.cs
@@ -8,29 +8,74 @@ using ProductionCalendarModel;
 
 namespace pCalendarService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class CalendarService : ICalendarService
     {
-        
+        CalendarContext db = new ProductionCalendarModel.CalendarContext();
 
+        public List<country> GetCountriesList()
+        {
+            return db.Countries.ToList<country>();
+        }
 
+        public int AddCountry(country Country)
+        {
+            throw new NotImplementedException();
+        }
 
-        //public string GetData(int value)
-        //{
-        //    return string.Format("You entered: {0}", value);
-        //}
+        public bool EditCountry(country Country)
+        {
+            throw new NotImplementedException();
+        }
 
-        //public CompositeType GetDataUsingDataContract(CompositeType composite)
-        //{
-        //    if (composite == null)
-        //    {
-        //        throw new ArgumentNullException("composite");
-        //    }
-        //    if (composite.BoolValue)
-        //    {
-        //        composite.StringValue += "Suffix";
-        //    }
-        //    return composite;
-        //}
+        public List<calendar> GetCalendarsList()
+        {
+            return db.Calendars.ToList<calendar>();
+        }
+
+        public int AddCalendar(calendar Calendar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditCalenadr(calendar Calendar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<dtype> GetDayTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<holiday> GetHolidaysList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddHoliday(holiday Holiday)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditHoliday(holiday Holiday)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<region> GetRegionsList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddRegion(region Region)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditRegion(region Region)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ pCalendarService/pCalendarServiceHost/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace pCalendarServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost svcHost = null;
+            try
+            {
+                var service = new pCalendarService.CalendarService();
+                svcHost = new ServiceHost(service);
+                svcHost.Open();
+            }
+            catch (Exception ex)
+            {
+                svcHost = null;
+                Console.WriteLine("Service can not be started \n\nError Message [" + ex.Message + "]");
+                Console.WriteLine("\nPress any key to close the Service");
+                Console.ReadKey();
+            }
+            if (svcHost != null)
+            {
+                Console.WriteLine("\nPress any key to close the Service");
+                Console.ReadKey();
+                svcHost.Close();
+                svcHost = null;
+            }
+        }
+    }
+}
